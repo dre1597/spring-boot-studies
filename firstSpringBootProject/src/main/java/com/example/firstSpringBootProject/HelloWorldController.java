b5bf13d559ef8284b4b613c412ccd08c4695fd03
André Pereira <andrepereira97@yahoo.com>
2022-11-27 22:34:41 -0300

    :tada: start first spring boot project

---
+++ firstSpringBootProject/src/main/java/com/example/firstSpringBootProject/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.firstSpringBootProject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+  @RequestMapping("/")
+  public String home() {
+    return "Hello World";
+  }
+}
