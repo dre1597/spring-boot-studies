2109ff1d579f7f1fe0ec13cd12f1c36107791882
André Pereira <andrepereira97@yahoo.com>
2023-01-28 13:52:32 -0300

    :tada: start graphql project from the spring docs

---
+++ graphql-from-docs/src/main/java/com/example/graphqlfromdocs/GraphqlFromDocsApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphqlfromdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlFromDocsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GraphqlFromDocsApplication.class, args);
+  }
+
+}
