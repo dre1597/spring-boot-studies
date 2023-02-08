ac05c6e89a6e1bb7de1a71b197e690dca5a6935e
André Pereira <andrepereira97@yahoo.com>
2023-02-08 01:06:11 -0300

    :sparkles: add constructors to request body classes

--- springboot2-essentials/src/main/java/com/example/springboot2essentials/request/AnimePostRequestBody.java
+++ springboot2-essentials/src/main/java/com/example/springboot2essentials/request/AnimePostRequestBody.java
@@ -1,12 +1,16 @@
 package com.example.springboot2essentials.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnimePostRequestBody {
   @NotBlank(message = "The anime name connot be empty")
   private String name;
--- springboot2-essentials/src/main/java/com/example/springboot2essentials/request/AnimePutRequestBody.java
+++ springboot2-essentials/src/main/java/com/example/springboot2essentials/request/AnimePutRequestBody.java
@@ -1,7 +1,9 @@
 package com.example.springboot2essentials.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
@@ -9,6 +11,8 @@ import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnimePutRequestBody {
 
   @NotNull(message = "The anime id connot be null")
