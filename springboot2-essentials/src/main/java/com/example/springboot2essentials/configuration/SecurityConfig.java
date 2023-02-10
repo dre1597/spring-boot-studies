454dd5122b5ec8be16a5363f4c5ddbc7fdc295bc
André Pereira <andrepereira97@yahoo.com>
2023-02-10 00:29:57 -0300

    :sparkles: add roles animes to routes

--- springboot2-essentials/src/main/java/com/example/springboot2essentials/configuration/SecurityConfig.java
+++ springboot2-essentials/src/main/java/com/example/springboot2essentials/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.springboot2essentials.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,6 +10,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Value("${security.default-password}")
   private String defaultPassword;
--- springboot2-essentials/src/main/java/com/example/springboot2essentials/controller/AnimeController.java
+++ springboot2-essentials/src/main/java/com/example/springboot2essentials/controller/AnimeController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -41,18 +42,20 @@ public class AnimeController {
   }
 
   @PostMapping
+  @PreAuthorize("hasRole('ADMIN')")
   public ResponseEntity<Anime> save(@RequestBody @Valid AnimePostRequestBody animePostRequestBody) {
     return new ResponseEntity<>(animeService.save(animePostRequestBody), HttpStatus.CREATED);
   }
 
   @PutMapping
-  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @PreAuthorize("hasRole('ADMIN')")
   public ResponseEntity<Void> update(@RequestBody @Valid AnimePutRequestBody animePutRequestBody) {
     animeService.replace(animePutRequestBody);
     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
   }
 
   @DeleteMapping(path = "/{id}")
+  @PreAuthorize("hasRole('ADMIN')")
   public ResponseEntity<Void> delete(@PathVariable long id) {
     animeService.delete(id);
     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
